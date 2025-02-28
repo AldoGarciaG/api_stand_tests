274ed92745679a925637730f68862e79c2c84b4e
Aldo Antonio Garcia Gomez <aldo.garciag@live.com>
2025-02-28 00:21:44 -0600

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-e83e834e-ea85-42ba-9bd0-d684d3618e1c.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
